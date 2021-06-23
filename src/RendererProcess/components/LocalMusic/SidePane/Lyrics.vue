528a6429a697d7fd8affba891e1f836748e2d122
Patrick <pntxall100@gmail.com>
2021-06-23 07:14:52 +0300

    Added AutoScroll controls

--- src/RendererProcess/components/LocalMusic/SidePane/Lyrics.vue
+++ src/RendererProcess/components/LocalMusic/SidePane/Lyrics.vue
@@ -20,14 +20,50 @@
         </pre>
       </div>
     </div>
+    <div class="lyr_controls">
+      <base-button
+        @click.native="offset += 10"
+        :icon="require('@/RendererProcess/assets/images/x.svg')"
+        id="off_add"
+      />
+      <p>{{ offset }}</p>
+      <base-button
+        @click.native="offset -= 10"
+        :icon="require('@/RendererProcess/assets/images/windowMinimize.svg')"
+        id="off_minus"
+      />
+      <base-button
+        @click.native="autoScroll = !autoScroll"
+        text="Auto Scroll"
+        :active="autoScroll"
+        id="autoScroll"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import BaseButton from "../../BaseComponents/BaseButton.vue";
 export default {
+  components: {
+    BaseButton,
+  },
   methods: {
     ...mapActions(["getLyrics"]),
+    autoScrollContainer() {
+      const audio = document.querySelector("audio");
+      const percent = audio.currentTime / audio.duration;
+      const container = document.querySelector(".LyricsContainer .lyrics");
+      const scrollTo = Math.floor(percent * container.scrollHeight);
+      container.scrollTo(0, scrollTo - this.offset);
+    },
+  },
+  data() {
+    return {
+      offset: 50,
+      autoScroll: false,
+    };
   },
   computed: {
     playingTrack() {
@@ -47,6 +83,12 @@ export default {
   },
   mounted() {
     this.getLyrics();
+    const audio = document.querySelector("audio");
+    document.querySelector("audio").addEventListener("timeupdate", (e) => {
+      if (this.autoScroll) {
+        this.autoScrollContainer();
+      }
+    });
   },
 };
 </script>
@@ -54,15 +96,17 @@ export default {
 <style lang="scss">
 .LyricsContainer {
   position: relative;
-  overflow: hidden;
-  height: 92%;
-  overflow-y: scroll;
+  scroll-behavior: smooth;
   width: 19vw;
+  height: 92%;
   .lyrics {
     position: absolute;
     font-family: roboto-light;
     text-align: left;
     padding: 10px;
+    overflow: hidden;
+    height: 80%;
+    overflow-y: scroll;
     div {
       padding: 10px;
       width: 250px;
@@ -78,6 +122,32 @@ export default {
       }
     }
     // width: 100px;
+  }
+  .lyr_controls {
+    position: absolute;
+    bottom: 0px;
+    width: 100%;
+    z-index: 5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    #off_add {
+      transform: rotate(45deg) scale(0.6);
+      img {
+        transform: scale(0.8);
+      }
+    }
+    #off_minus {
+      transform: rotate(45deg) scale(0.6);
+      img {
+        transform: rotate(-45deg);
+      }
+    }
+    #autoScroll {
+      position: absolute;
+      top: 0px;
+      transform: translateY(-110%);
+    }
   }
 }
 </style>
