a9b6d5081b06f9eb647e52fbc4c586664a1a4f07
Eddy Otsutsuki <23711001+hypernova7@users.noreply.github.com>
2021-07-05 02:30:06 -0500

    Configuration files are added for code improvement and cleanup. And fixes (#15)
    * Added .editorconfig
    * Added .prettierrc.js
    * Added .eslintrc.js and .eslintignore
    
    * Fixed CHANGELOG.md format
    * Fixed git-chglog workflow
    * Fixed package.json
    
    * Updated git-chglog config
    * Updated tsconfig.json (esnext is unstable)
    * Updated vue.config.js (Added webpackbar for more elegant and disable default progress)
    * Updated renovate.json
    
    * Renamed deploy workflow to release and fixed

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,26 @@
+const WebpackBar = require('webpackbar');
+
 module.exports = {
-	pluginOptions: {
-		electronBuilder: {
-			nodeIntegration: true,
-			builderOptions: {
-				appId: "com.flb.flbmusic",
-				productName: "FLB Music",
-				copyright: "MIT",
-				publish: ["github"],
-			},
-		},
-	},
+  devServer: {
+    progress: false
+  },
+  configureWebpack: {
+    plugins: [
+      new WebpackBar({
+        name: 'Building FLB Music Player',
+        color: '#0389ff'
+      })
+    ]
+  },
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        appId: 'com.flb.flbmusic',
+        productName: 'FLB Music',
+        copyright: 'MIT',
+        publish: ['github']
+      }
+    }
+  }
 };
